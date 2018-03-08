b62577e07818170354d65a31cf061cb725c535d5
Guilherme Varela <guilhermevarela@hotmail.com>
2018-03-08 19:33:44 -0300

    week_3: lecture 08 added

--- week_3/08/recursion-3/src/evaluate.cpp
+++ week_3/08/recursion-3/src/evaluate.cpp
@@ -17,7 +17,7 @@ using namespace std;
 //evaluate("(1+(2*4))") --> 9
 //evaluate("((1+3)+((2*4)*5))") --> 9
 int evaluate(string expression);
-int evaluate_rec(string expression, int index);
+int evaluate_rec(const string& expression, int& index);
 
 int main() {
     cout << "evaluate(\"7\") : " << evaluate("7")  << endl;
@@ -28,42 +28,30 @@ int main() {
 }
 
 int evaluate(string expression) {
-
+    int index= 0;
+    return evaluate_rec(expression, index);
 }
 
-int findClosingParanthesis(string exp, int index) {
-    int l = exp.length();
-    int pos = l;
-    for (int i=index+1 ; i < l; i++) {
-        if (expression[i] == ')') {
-            pos=i;
-            break
+
+int evaluate_rec(const string& expression, int& index) {
+    char ch= expression[index];
+    if  (ch == '(') {
+        index++; //move past (
+        int left= evaluate_rec(expression, index);
+        char opr = expression[index];
+        index++;
+        int right= evaluate_rec(expression, index);
+        index++; //move from ) character
+        if (opr == '+') {
+            return left + right;
+        } else {
+            return left * right;
         }
+    } else  {
+        index++;
+        return charToInteger(ch);
     }
-    return pos
 }
-int evaluate_rec(string expression, int index) {
-    if (ch == '+'){
-        return evaluate_rec(expression.substr(0,index-1), index/2) +
-                evaluate_rec(expression.substr(index+1,l),(l-index)/2);
-    } else if (ch == 'x') {
-        return evaluate_rec(expression.substr(0,index-1), index/2) +
-                evaluate_rec(expression.substr(index+1,l),(l-index)/2);
-    }
-   // must be an opening parenthesis
-   int pos=findClosingParanthesis(expression, index)
-   return evaluate_rec(expression.substr(index, pos) ,(index + pos)/2);
-}
-
-//int evaluate_sum(string expression) {
-//    if
-//    return 0
-//}
-//int evaluate_multiplication(string expression) {
-//    return 0
-//}
-
-
 
 
 
